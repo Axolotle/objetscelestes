4e6d6a98ad76339e57811be3871f86115ade191d
Axolotle <nchesnais@mailoo.org>
2019-12-02 16:08:49 +0100

    improved key switching

--- website/js/editor/components/abstract/Keyboard.js
+++ website/js/editor/components/abstract/Keyboard.js
@@ -4,7 +4,7 @@ import { Vector3 } from '../../../libs/three.module.js';
 
 const _VECZERO = new Vector3();
 const _keys = {
-    move: ['KeyA', 'KeyD', 'KeyW', 'KeyS'],
+    move: ['KeyA', 'KeyD', 'KeyS', 'KeyW'],
     roll: ['KeyQ', 'KeyE']
 }
 let _onkeydown, _onkeyup;
@@ -26,47 +26,44 @@ export class Keyboard extends Subscriber {
             this.vector.z = e.code === _keys.roll[0] ? -1 : 1;
         } else if (_keys.move.includes(e.code)) {
             switch (e.code) {
-                case 'KeyA': this.vector.x = -1; break;
-                case 'KeyD': this.vector.x =  1; break;
-
-                case 'KeyW': this.vector.y =  1; break;
-                case 'KeyS': this.vector.y = -1; break;
+                case _keys.move[0]: this.vector.x = -1; break;
+                case _keys.move[1]: this.vector.x =  1; break;
+                case _keys.move[2]: this.vector.y = -1; break;
+                case _keys.move[3]: this.vector.y =  1; break;
             }
         }
-        if (!this.keepSending) {
-            this.keepSending = true;
+        if (!this.animating) {
+            this.animating = true;
             this.animate();
         }
     }
 
     onkeyup(e) {
-        if (_keys.move.includes(e.code)) {
-            switch (e.code) {
-                case 'KeyA': this.vector.x = 0; break;
-                case 'KeyD': this.vector.x = 0; break;
-
-                case 'KeyW': this.vector.y = 0; break;
-                case 'KeyS': this.vector.y = 0; break;
-            }
-        } else if (_keys.roll.includes(e.code)) {
-            this.vector.z = 0;
-        }
-        if (this.vector.equals(_VECZERO)) {
-            this.keepSending = false;
+        switch (e.code) {
+            // pitch
+            case _keys.move[0]: this.vector.x === -1 ? this.vector.x = 0 : this.vector.x = 1; break;
+            case _keys.move[1]: this.vector.x ===  1 ? this.vector.x = 0 : this.vector.x = -1; break;
+            // yaw
+            case _keys.move[2]: this.vector.y === -1 ? this.vector.y = 0 : this.vector.y = 1; break;
+            case _keys.move[3]: this.vector.y ===  1 ? this.vector.y = 0 : this.vector.y = -1; break;
+            // roll
+            case _keys.roll[0]: this.vector.z === -1 ? this.vector.z = 0 : this.vector.z =  1; break;
+            case _keys.roll[1]: this.vector.z ===  1 ? this.vector.z = 0 : this.vector.z = -1; break;
         }
     }
 
     animate() {
-        if (this.keepSending) {
-            requestAnimationFrame(this.animate.bind(this));
-        } else {
+        if (this.vector.equals(_VECZERO)) {
+            this.animating = false;
             return;
+        } else {
+            requestAnimationFrame(this.animate.bind(this));
         }
 
-        if (!this.vector.z !== 0) {
+        if (this.vector.z !== 0) {
             this.publish('keyboard-roll', this.vector.z);
         }
-        if (!this.vector.x !== 0 || this.vector.y !== 0) {
+        if (this.vector.x !== 0 || this.vector.y !== 0) {
             this.publish('keyboard-drag', this.vector);
         }
     }
