439dded849ea35cb21b9857759ff5affe085df92
Axolotle <nchesnais@mailoo.org>
2019-12-21 18:12:09 +0100

    add SkyMapController to select/unselect asterism in SkyMap objects

---
+++ website/js/editor/controllers/SkyMapController.js
@@ -0,0 +1,108 @@
+import { Color } from '../../libs/three.module.js';
+
+import { SkyMap } from '../../objects3d/SkyMap.js';
+import { Asterism } from '../../objects3d/Asterism.js';
+import { AsterismController } from './AsterismController.js';
+
+
+const _renderColor = new Color(0x000000).toArray();
+const _selectColor = new Color(0xff00ff).toArray();
+
+
+export class SkyMapController {
+    constructor() {
+        this.object = undefined;
+        this.asterismCtrl = new AsterismController();
+        this.selected = [];
+    }
+
+    set(object) {
+        if (this.object !== undefined) this.dispose();
+        this.object = object;
+        this.asterismCtrl.object = undefined;
+        this.selected.length = 0;
+    }
+
+    raycast(raycaster) {
+        let intersect = raycaster.intersectObject(this.object, true)[0];
+        if (intersect !== undefined) {
+            return {
+                asterism: this.object.children.indexOf(intersect.object),
+                segment: intersect.index
+            }
+        } else {
+            return null;
+        }
+    }
+
+    select(targets, shift) {
+        let asterism = this.object.children[targets.asterism];
+        // Asterism is already selected
+        if (this.selected.includes(targets.asterism)) {
+            // Asterism is currently controlled: ask for segment selection.
+            if (this.asterismCtrl.object === asterism && this.selected.length === 1) {
+                console.log('same');
+                this.asterismCtrl.select(targets.segment, shift);
+            // Multiple asterism are selected
+            } else if (this.selected.length > 1) {
+                // Is combinatory: unselect targeted asterism.
+                if (shift) {
+                    this.unselect(targets.asterism);
+                    this.asterismCtrl.set(this.object.children[this.selected[this.selected.length - 1]]);
+                // Unselect everything but the targeted on and select segment
+                } else {
+                    this.selected.splice(this.selected.indexOf(targets.asterism), 1);
+                    this.unselect();
+                    this.asterismCtrl.set(asterism);
+                    this.asterismCtrl.select(targets.segment);
+                    this.selected = [targets.asterism];
+                }
+            }
+        // Asterism is not selected
+        } else {
+            // Is non-combinatory: unselect previous selection and control the new target
+            if (!shift) {
+                this.asterismCtrl.set(asterism);
+                this.unselect();
+            // Asterism is controlled: unselect it and free the controller
+            } else if (this.asterismCtrl.object !== undefined) {
+                this.asterismCtrl.unselect();
+                this.asterismCtrl.clear();
+            }
+            // Add the targeted asterism to selection
+            asterism.fillAttribute('color', _selectColor, 0, asterism.path.length);
+            this.selected.push(targets.asterism);
+        }
+
+    }
+
+    unselect(index) {
+        if (index !== undefined) {
+            let asterism = this.object.children[index];
+            asterism.fillAttribute('color', _renderColor, 0, asterism.path.length);
+            this.selected.splice(this.selected.indexOf(index), 1);
+        } else {
+            // Controlled asterism has segments selected: unselect those.
+            if (this.asterismCtrl.selected.length > 0) {
+                this.asterismCtrl.unselect();
+            // Clear controller and unselect previous selection.
+            } else {
+                for (const i of this.selected) {
+                    this.object.children[i].fillAttribute('color', _renderColor, 0, this.object.children[i].path.length);
+                }
+                this.selected.length = 0;
+            }
+
+        }
+    }
+
+    dispose() {
+        for (const child of this.object.children) {
+            this.object.remove(child);
+            child.geometry.dispose();
+            child.material.dispose();
+        }
+
+        this.object.parent.remove(this.object);
+    }
+}
