f62898dbef9bf068a961b22eb137099bdf48c545
Axolotle <nchesnais@mailoo.org>
2020-01-09 13:14:34 +0100

    add DoubleRange custom element

--- website/js/ui/components/index.js
+++ website/js/ui/components/index.js
@@ -1,6 +1,5 @@
 export { Canvas3D } from './domElements/Canvas3D.js';
-export { SingleRange } from './inputs/Ranges.js';
-export { IntervalRange, IntervalRangeNumber } from './Ranges.js';
+export { SingleRange, DoubleRange } from './inputs/Ranges.js';
 export { ButtonSwitch } from './domElements/Switch.js';
 export { Card } from './domElements/Card.js';
 export { Select } from './domElements/Select.js';
--- website/js/ui/components/inputs/Ranges.js
+++ website/js/ui/components/inputs/Ranges.js
@@ -106,4 +106,121 @@ export class SingleRange extends Range {
 }
 
 
+export class DoubleRange extends Range {
+    static get properties() {
+        const prop ={
+            ...Range.properties,
+            valueLow: { type: Number },
+            valueHigh: { type: Number },
+            value: { type: Array },
+        }
+        console.log(prop);
+        return prop ;
+    }
+
+    static get styles() {
+        return [
+            super.styles,
+            css`
+                input.original {
+                    position: absolute;
+                    width: calc(100% - 2rem - 2px);
+                }
+                input.original::-moz-range-thumb {
+                    transform: scale(1); /* FF doesn't apply position it seems */
+                    z-index: 1;
+                }
+                input.original::-webkit-slider-thumb {
+                    position: relative;
+                    z-index: 2;
+                }
+
+                input.ghost {
+                    position: relative;
+                    background: var(--track-background);
+                    --track-background: linear-gradient(to right,
+                            transparent var(--low), var(--range-color) 0,
+                            var(--range-color) var(--high), transparent 0
+                        ) no-repeat 0 50% / 100% 100%;
+                    --range-color: hsl(240, 100%, 100%);
+                }
+                input.ghost::-moz-range-track {
+                    background: var(--track-background);
+                }
+                input.ghost::-webkit-slider-runnable-track {
+                    background: var(--track-background);
+                }
+            `
+        ];
+    }
+
+    get valueLow() { return Math.min(this.original ? this.original.value : this.min, this.ghost ? this.ghost.value : this.max); }
+    set valueLow(value) { this.original.value = value; }
+
+    get valueHigh() { return Math.max(this.original ? this.original.value : this.min, this.ghost ? this.ghost.value : this.max); }
+    set valueHigh(value) { this.ghost.value = value; }
+
+    get value() { return [this.valueLow, this.valueHigh] }
+    set value(values) {
+        this.original.value = values[0];
+        this.ghost.value = values[1];
+    }
+
+    constructor() {
+        super();
+        this.updateComplete.then(() => {
+            this.original = this.shadowRoot.querySelector('.original');
+            this.ghost = this.shadowRoot.querySelector('.ghost');
+            this.valueLow = this.min;
+            this.valueHigh = this.max;
+            this.updateStyle();
+        });
+    }
+
+    onInput() {
+        this.updateStyle();
+        const ev = new CustomEvent('change', { detail: {value: this.value}});
+        console.log(this.value);
+        this.dispatchEvent(ev);
+    }
+
+    onMousedown(e) {
+        // Find the horizontal position that was clicked
+        let clickValue = this.min + (this.max - this.min) * e.offsetX / this.ghost.offsetWidth;
+        let middleValue = (this.valueHigh + this.valueLow)/2;
+        if ( (this.valueLow == this.ghost.value) == (clickValue > middleValue) ) {
+            // Click is closer to input element and we swap thumbs
+            this.original.value = this.ghost.value;
+        }
+    }
+
+    updateStyle() {
+        this.ghost.style.setProperty(
+            "--low",
+            100 * ((this.valueLow - this.min) / (this.max - this.min)) + 1 + "%"
+        );
+        this.ghost.style.setProperty(
+            "--high",
+            100 * ((this.valueHigh - this.min) / (this.max - this.min)) - 1 + "%"
+        );
+    }
+
+    render() {
+        return html`
+            <input
+                @input="${this.onInput}"
+                class="original" type="range"
+                min="${this.min}" max="${this.max}" step="${this.step}"
+            />
+            <input
+                @input="${this.onInput}" @mousedown="${this.onMousedown}"
+                class="ghost" type="range"
+                min="${this.min}" max="${this.max}" step="${this.step}"
+            />
+        `;
+    }
+}
+
+
 customElements.define('single-range', SingleRange);
+customElements.define('double-range', DoubleRange);
